0cfe5a01334b9ed4f7fb0c65c02efdf620c79500
hirowo <34668037+hirowo@users.noreply.github.com>
2020-08-21 17:09:50 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ namespace comment {
         pin2: DigitalPin;        
     }
     
-    //% blockId=INIT block="初期化"
+    //% blockId=INIT block="初期化　%v"
     export function init() :KAGA_IoT{
         let block = new KAGA_IoT();
         serial.redirect(SerialPin.P0, SerialPin.P1, 9600)
