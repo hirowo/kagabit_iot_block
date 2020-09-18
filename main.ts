dceb8a843b028744be912866eeefbe0d587a81ae
hirowo <34668037+hirowo@users.noreply.github.com>
2020-09-18 19:53:58 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -55,7 +55,7 @@ namespace comment {
         serial.writeString(key);
         serial.writeString("\n");
     }
-    //% blockId=SET_AM block="Ambient送信データをセット %v %String "
+    //% blockId=SET_AM block="Ambient送信データをセット %nunver %String "
     export function SetAmb(channel : number,data : string) {
         serial.writeString("STA ");
         serial.writeString(channel.toString());
