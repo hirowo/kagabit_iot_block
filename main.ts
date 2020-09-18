7736019fa4d663399a3971f9ddfdb0357f8a30fd
hirowo <34668037+hirowo@users.noreply.github.com>
2020-09-18 19:09:56 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ namespace comment {
 //        pin2: DigitalPin;        
     }
     
-    //% blockId=INIT block="初期化　"
+    //% blockId=INIT block="初期化　    "
     export function init() : void{
         let block = new KAGA_IoT();
         serial.redirect(SerialPin.P0, SerialPin.P1, 9600)
@@ -18,13 +18,13 @@ namespace comment {
         serial.writeString(ssid);
         serial.writeString("\n");
     }
-    //% blockId=PASS block="PASSWORD %string %v"
+    //% blockId=PASS block="PASSWORD %string"
     export function sendPASS(pass : string){
         serial.writeString("PA ");
         serial.writeString(pass);
         serial.writeString("\n");
     }
-   //% blockId=SCONNECT block="接続  %v"
+   //% blockId=SCONNECT block="接続       "
     export function Connect() : void {
         serial.writeString("WS");
         serial.writeString("\n");
@@ -36,18 +36,18 @@ namespace comment {
         serial.writeString(interval.toString());
         serial.writeString("\n");
     }
-     //% blockId=mdsn block="ホスト名を設定 %string %v"
+     //% blockId=mdsn block="ホスト名を設定 %string "
     export function set_mdsn(str : string) {
         serial.writeString("MD ");
         serial.writeString(str);
         serial.writeString("\n");
     }
-    //% blockId=S_web block="webサーバー開始 %v"
+    //% blockId=S_web block="webサーバー開始 "
     export function start_web() : void {
         serial.writeString("SWEB");
         serial.writeString("\n");
     }
-    //% blockId=START_AMB block="Ambientサービスに接続 %String %String %v"
+    //% blockId=START_AMB block="Ambientサービスに接続 %String %String "
     export function startAmb(ambient_id : string,key : string) {
         serial.writeString("SAMB ");
         serial.writeString(ambient_id);
@@ -55,7 +55,7 @@ namespace comment {
         serial.writeString(key);
         serial.writeString("\n");
     }
-    //% blockId=SET_AM block="Ambient送信データをセット %v %String %v"
+    //% blockId=SET_AM block="Ambient送信データをセット %v %String "
     export function SetAmb(channel : number,data : string) {
         serial.writeString("STA ");
         serial.writeString(channel.toString());
@@ -63,7 +63,7 @@ namespace comment {
         serial.writeString(data);
         serial.writeString("\n");
     }
-    //% blockId=SEND_AMB block="Ambientにデータを送信%v"
+    //% blockId=SEND_AMB block="Ambientにデータを送信"
     export function SendAmb()  : void{
         serial.writeString("SEA");
         serial.writeString("\n");
